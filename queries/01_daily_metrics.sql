d0c66e6179095811c84df541d7a7d5506136c1a3
Imon Deb <imondeb9@gmail.com>
2026-04-18 10:58:18 +0530

    Update 01_daily_metrics.sql

--- queries/01_daily_metrics.sql
+++ queries/01_daily_metrics.sql
@@ -1,3 +1,4 @@
+-- Daily sessions and revenue analysis
 SELECT PARSE_DATE('%Y%m%d', date) AS session_date,
   COUNT(*) AS total_sessions,
   SUM(totals.totalTransactionRevenue)/1000000 AS total_revenue
